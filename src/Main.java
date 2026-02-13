82d4642679085a7a2e2734ca3d93b4390e8d693d
JuliCaste <julianacastellanos0108@gmail.com>
2026-02-13 17:19:45 -0500

    Cambios git

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,11 @@ public class Main {
         System.out.println("Juliana Castellanos Vanegas");
         System.out.println("El semestre pasado vi el lenguaje: Python");
 
+        System.out.println("1.Comandos de git vistos");
+        System.out.println("1. git add .");
+        System.out.println("2. git commit -m");
+        System.out.println("3. git push");
+        System.out.println("3. git switch");
+
     }
 }
